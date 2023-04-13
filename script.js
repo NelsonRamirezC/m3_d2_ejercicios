132e652238cdc8a1f5aff75cf26447a32b9ff604
Nelson Ramirez <xhyskoax@gmail.com>
2023-04-13 12:34:52 -0400

    Agregando ejercicio con moment.js

---
+++ script.js
@@ -0,0 +1,21 @@
+let fechaNacimiento = moment("1990-01-01");
+
+let fecha = document.querySelector("#fecha");
+
+fecha.innerText = fechaNacimiento.format("DD-MM-YYYY");
+
+let calendario = document.getElementById("calendario");
+
+calendario.addEventListener("change", function () {
+    let fechaAgendada = moment(calendario.value);
+    let fechaHoy = moment();
+    let diferencia = fechaAgendada.diff(fechaHoy, "days");
+    console.log(diferencia);
+    if (diferencia < 0) {
+        alert("No puedo elegir una fecha inferior a la fecha actual.");
+        reserva.value = fechaHoy.format("YYYY-MM-DD");
+    } else {
+        reserva.value = fechaAgendada = calendario.value;
+        fechaPedido.innerText = diferencia;
+    }
+});
